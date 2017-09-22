470dd26c3a69ed67b4185e12790d68a6015f4450
Andrew Before <andrew.before@HCL.COM>
2017-09-22 09:36:35 -0700

    angualr practice

--- angular/hello-world/src/app/app.module.ts
+++ angular/hello-world/src/app/app.module.ts
@@ -2,20 +2,26 @@ import { AuthorsService } from './authors/authors.service';
 import { CoursesService } from './courses.service';
 import { CoursesComponent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
 import { AuthorComponent } from './author/author.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { TitleCasePipe } from './title-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     CourseComponent,
-    AuthorComponent
+    AuthorComponent,
+    FavoriteComponent,
+    TitleCasePipe
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [CoursesService,
