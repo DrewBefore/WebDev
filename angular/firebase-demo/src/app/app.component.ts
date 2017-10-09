da846291b92404d874e258b0431b5c895772b530
Andrew Before <andrew.before@HCL.COM>
2017-10-09 14:33:16 -0700

    firebase example

--- angular/firebase-demo/src/app/app.component.ts
+++ angular/firebase-demo/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  courses$: FirebaseListObservable<any[]>;
+  course$;
+  author$;
+
+  constructor(private db: AngularFireDatabase) {
+    this.courses$ = db.list('courses');
+    this.course$ = db.object('/courses/1');
+    this.author$ = db.object('/authors/1');
+  }
+
+  add(course: HTMLInputElement) {
+    this.courses$.push(course.value);
+    course.value = '';
+  }
+
+  update(course) {
+    this.db.object('/courses/' + course.$key).set({
+      title: course.$value + "updated",
+      price: 150
+    });
+  }
+
+  delete(course) {
+    this.db.object('/courses/' + course.$key).remove()
+    .then(x => console.log("deleted"));
+  }
 }
